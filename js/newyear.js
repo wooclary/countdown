26c728e6a958ad5e027adaa742c8a12d2607ec8b
wooclary <woo-henry@hotmail.com>
2021-12-31 10:47:06 +0800

    test

--- js/newyear.js
+++ js/newyear.js
@@ -134,8 +134,8 @@ $(document).ready(function() {
 		callbacks: {
 			interval: function() {
 				var time = this.factory.getTime().time;
-				if(time <= 10 && time > 0) {
-					pulse();
+				if(time <= 100000 && time > 0) {
+					celebrate();
 				}
 				else if(time <= 0) {
 					celebrate();
